f70e5127a93cbf83ff006a5f08c859be105681ce
Cydeo-Saim <saim@cybertekschool.com>
2024-03-20 09:01:12 -0500

    week 4 quiz

---
+++ src/week4/VariableShortQuiz.java
@@ -0,0 +1,115 @@
+package week4;
+
+public class VariableShortQuiz {
+    public static void main(String[] args) {
+
+        System.out.println("=============== Question 1 ===============");
+        // Which method is used to define the entry point of a Java program?
+
+        // main()
+
+        System.out.println("=============== Question 2 ===============");
+        // Which method is used to print a message to the standard output stream without a newline?
+
+        // System.out.print()
+
+        System.out.println("=============== Question 3 ===============");
+        // Which escape sequence is used to represent a single backslash character?
+
+        // \\
+
+        System.out.println("=============== Question 4 ===============");
+        // Which of the following code fragment prints "I can't wait to learn Java!"?
+
+        /*
+            public class HelloWorld {
+                public static void main(String[] args) {
+                    System.out.println("I can't wait to learn Java!");
+                }
+            }
+        */
+
+        System.out.println("=============== Question 5 ===============");
+        // Which type of comment is used to comment out a single line of code?
+
+        // Single-line comment (//)
+
+        System.out.println("=============== Question 6 ===============");
+
+        /*
+        System.out.print("Hello ");
+        System.out.println("Cydeo");
+        System.out.print("Welcome ");
+        System.out.println("to Java");
+         */
+
+        // Which of the following has the exact same output as the code statements above?
+
+        System.out.print("Hello ");
+        System.out.print("Cydeo\n");
+        System.out.print("Welcome ");
+        System.out.print("to Java\n");
+
+        System.out.println("=============== Question 7 ===============");
+        // What is the purpose of a variable in Java?
+
+        // To store and manipulate data of a specific type.
+
+        System.out.println("=============== Question 8 ===============");
+        // What is the data type?
+
+        // Defines the kind of values that variables can hold.
+
+        System.out.println("=============== Question 9 ===============");
+        // Which of the following is NOT a Java primitive data type?
+
+        // String
+
+        System.out.println("=============== Question 10 ===============");
+        // Which of the following answers does the number of bits increase from fewest (on the left) to most (on the right)?
+
+        // byte -  short - int - long
+
+        System.out.println("=============== Question 11 ===============");
+        // Which data types can store whole (integer) numbers?
+
+        // byte, short, int, long
+
+        System.out.println("=============== Question 12 ===============");
+        // Which one of the following code fragments will cause an error?
+
+//        int b;
+//        int b = 5;
+
+        System.out.println("=============== Question 13 ===============");
+        // Which ones are correct about the Java naming convention? (Choose all that apply)
+
+        /*
+            - After the first character, you may use the letters a-z or A-Z, the digits 0-9, underscores(_), or dollar sign($).
+            - The first character must be one of the letters a-z or A-Z, an underscore(_), or a dollar sign($).
+            - Uppercase and lowercase characters are distinct. This means itemsOrdered is not the same as itemsordered.
+         */
+
+        System.out.println("=============== Question 14 ===============");
+        // What will be the output of this code fragment?
+
+        System.out.println("Result A" + 0);
+        System.out.println("Result B" + 1);
+
+        /*
+            Result A0
+            Result B1
+         */
+
+        System.out.println("=============== Question 15 ===============");
+        // What will be the output of this code fragment?
+        System.out.println("5 - 2 = " + 3);
+        System.out.println("5 + 2 = " + 7);
+
+        /*
+            5 - 2 = 3
+            5 + 2 = 7
+         */
+
+    }
+}

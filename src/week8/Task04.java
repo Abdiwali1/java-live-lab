80a6db4e61335071b1d1bf7c6b572c4dfed64bf5
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-04-17 13:14:12 -0400

    task solutions are added.

---
+++ src/week8/Task04.java
@@ -0,0 +1,19 @@
+package week8;
+
+import java.util.*;
+
+public class Task04 {
+
+    public static void main(String[] args) {
+        int[] input = {4, 3, 1, 4, 5, 2, 4, 8, 4, 8};
+        System.out.println(secondMax(input));
+    }
+
+    public static int secondMax(int[] input){
+        int secondMax = 0;
+        int max = 0;
+
+        Arrays.sort(input);
+        return secondMax;
+    }
+}

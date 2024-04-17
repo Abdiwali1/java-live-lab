2645014f76693453dd3aade4191aea91efafb75f
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-04-17 12:00:52 -0400

    task solutions are added.

---
+++ src/week8/Task01.java
@@ -0,0 +1,36 @@
+package week8;
+
+import java.util.*;
+
+public class Task01 {
+
+    /*
+    java 1
+    python 2
+    ruby 2
+    c# 1
+     */
+
+    public static void main(String[] args) {
+
+        String[] inputArray = {"Java", "Python", "Python", "Ruby", "Ruby", "C#"};
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < inputArray.length; i++) {
+            int count = 0;
+            for (int j = 0; j < inputArray.length; j++) {
+                if(inputArray[i].equals(inputArray[j])){
+                    count ++;
+                }
+            }
+            if (count == 1){
+               result.add(inputArray[i]);
+               System.out.println(inputArray[i]);
+            }
+
+        }
+        System.out.println(result);
+
+
+    }
+}
---
+++ src/week8/Task02.java
@@ -0,0 +1,37 @@
+package week8;
+
+public class Task02 {
+
+    public static void main(String[] args) {
+        int[][] array = {
+                {3, 4, 5, 6},
+                {5, 2, 6},
+                {10, 40, 20}};
+        printMultiDiArray(array);
+    }
+
+
+    public static void printMultiDiArray(int[][] array){
+
+        String result = "";
+        /*
+            index 0 | (3)(4)(5)(6)
+            index 1 | (5)(2)(6)
+            index 2 | (10)(40)(20)
+         */
+
+        // iterate through 2d array
+        for (int i = 0; i < array.length; i++) {
+            result += "index " + i + "| (";
+
+            for (int j = 0; j < array[i].length; j++) {
+                result += array[i][j];  // 3    4
+                if (j < array[i].length - 1){
+                    result += ")(";
+                }
+            }
+            result += ")\n";
+        }
+        System.out.println(result);
+    }
+}
